bf92e62d6cfa6599152e6ff575c9c93ed39a2e73
Irina Dumitrascu <irina2000d@yahoo.com>
2009-04-12 01:43:37 +0200

    post: subject scraping logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,37 @@
 class Post < ActiveRecord::Base
   belongs_to :group
   enum_field "kind", [ "offer", "offer_completed", "request", "request_completed", "ignore" ]
+
+  def self.parse_subject(subject_original)
+    post = {}
+    subject = subject_original
+    # remove [Freecycle Bucuresti] heading
+    match = subject.match /^\[Freecycle [^\]]*\] (.*)/
+    if match
+      subject = match[1]
+    end
+    # have kind?
+    kinds =
+      {
+        'ofer' => 'offer',
+        'dat'  => 'offer_completed',
+        'caut' => 'request',
+        'luat' => 'request_completed'
+      }
+
+    expr = /\[?(caut|ofer|dat|luat)\]?\:?/i
+    match = subject.match expr
+    if (match)
+      post[:kind] = kinds[match[1].downcase]
+      subject[expr] = ''
+    end
+    post[:subject] = subject.strip
+    return post
+  end
+
+  def self.offuscate_author(post)
+    require 'md5'
+    post.author_md5 = MD5.new(post.author_md5)
+    post.save
+  end
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -12,4 +12,27 @@ class PostTest < ActiveSupport::TestCase
   
   should_belong_to :group
   
+  context 'setup from scraping:' do
+    [
+      {:subject_original => "[Freecycle Bucuresti] OFER: frigider vechi", :kind => 'offer', :subject => "frigider vechi"},
+      {:subject_original => "[Freecycle Bucuresti] CAUT: blender", :kind => 'request', :subject => "blender"},
+      {:subject_original => "[Freecycle Bucuresti] DAT: Stephen King", :kind => "offer_completed", :subject => "Stephen King"},
+      {:subject_original => "[Freecycle Bucuresti] LUAT: carcasa miniATX", :kind => "request_completed", :subject => "carcasa miniATX"},
+      {:subject_original => "CAUT: Cablu acceleratie Dacia Break 1410 din 1997", :kind => 'request', :subject => "Cablu acceleratie Dacia Break 1410 din 1997"},
+      {:subject_original => "[OFER] monitor de 20\" HP M900 functional", :kind => "offer", :subject => "monitor de 20\" HP M900 functional"},
+      {:subject_original => "carti", :kind => nil, :subject => "carti"},
+    ].each do |test|
+      context "parse \"#{test[:subject_original]}\"" do
+        setup do
+          @post = Post.parse_subject(test[:subject_original])
+        end
+        should "get subject right" do
+          assert_equal test[:subject], @post[:subject]
+        end
+        should "get kind right" do
+          assert_equal test[:kind], @post[:kind]
+        end
+      end
+    end
+  end
 end
