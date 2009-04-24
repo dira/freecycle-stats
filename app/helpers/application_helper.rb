c89ddb681f0367f5de000cb3b8e103bcfd8c8e64
Irina Dumitrascu <irina2000d@yahoo.com>
2009-04-24 21:38:47 +0200

    worked on look & feel - basic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def html_title
+    title = APP_CONFIG[:site_name].dup
+    title << ": #{@page_title}" if @page_title
+    title
+  end
 end
