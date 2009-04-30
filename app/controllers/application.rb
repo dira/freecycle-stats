7bc5d1b0a8e5f21c7e7e250b4b5ef18f06113e74
Irina Dumitrascu <irina2000d@yahoo.com>
2009-04-30 19:16:53 +0200

    protected helper method in app controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -5,13 +5,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   before_filter :set_locale
 
-  def set_locale
-    locale = params[:locale] || 'ro-RO'
-    I18n.locale = locale
-    I18n.load_path += Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ]
-  end
-
-
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'c9fdd414e966d5fb5c712f2a2311bc52'
@@ -20,4 +13,11 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+private
+  def set_locale
+    locale = params[:locale] || 'ro-RO'
+    I18n.locale = locale
+    I18n.load_path += Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ]
+  end
 end
