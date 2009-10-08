0ce13ffe8c618d05e25dd90d297b393b5e40439a
Irina Dumitrascu <me@dira.ro>
2009-10-08 12:36:49 +0300

    Moved to imap. Faster and more control

--- app/scripts/get_posts.rb
+++ app/scripts/get_posts.rb
@@ -1,17 +1,44 @@
-require 'tmail'
-require 'net/pop_ssl'
+require 'net/imap'
 require 'pp'
 
 class MailHeaderFetcher
+  LABEL_NEW = "#{RAILS_ENV}/new"
+  LABEL_FETCHED = "#{RAILS_ENV}/fetched"
+
   def initialize(server, port, username, password)
-    @server, @port, @username, @password = server, port, username, password
+    @imap = Net::IMAP.new(server, port, true)
+    @imap.login(username, password)
   end
   
-  def fetch_headers
-    Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
-    Net::POP3.start(@server, @port, @username, @password) do |pop|
-      pop.mails.map { |pop_mail| TMail::Mail.parse(pop_mail.header) }
+  def retrieve_messages
+    @imap.select(LABEL_NEW)
+
+    unfetched = @imap.uid_search("ALL")
+    pp "#{unfetched.size} new messages"
+
+    unfetched.each_slice(50) do |uids|
+      to_move = []
+
+      @imap.uid_fetch(uids, "ENVELOPE").each do |envelope|
+        ok = yield envelope.attr["ENVELOPE"]
+        to_move << envelope.attr["UID"] if ok
+      end
+
+      @imap.uid_copy(to_move, LABEL_FETCHED)
+      @imap.uid_store(to_move, "+FLAGS", [:Deleted])
+      flush
     end
+
+    disconnect
+  end
+
+  def flush
+    @imap.expunge
+  end
+
+  def disconnect
+    @imap.logout()
+    @imap.disconnect() rescue nil
   end
 end
 
@@ -26,16 +53,20 @@ class GetPosts
     username = MAIL_CONFIG[group.name]['username']
     password = MAIL_CONFIG[group.name]['password']
     
-    mails = MailHeaderFetcher.new(server, port, username, password).fetch_headers
-    pp "#{mails.size} mails"
-    mails.each do |mail|
-      hash = Post.parse_subject(mail.subject)
-      hash[:group_id] = group.id
-      hash[:message_id] = mail.message_id
-      from = mail.from[0] rescue ''
-      hash[:author_md5] = Post.obfuscate_author(from)
-      hash[:sent_date] = mail.date 
-      Post.find_or_create_by_message_id(hash) 
+    MailHeaderFetcher.new(server, port, username, password).retrieve_messages do |mail|
+      begin
+        hash = Post.parse_subject(mail.subject)
+        hash[:group_id] = group.id
+        hash[:message_id] = mail.message_id
+        from = "#{mail.from[0].mailbox || mail.from[0].name}@#{mail.from[0].host}"
+        hash[:author_md5] = Post.obfuscate_author(from)
+        hash[:sent_date] = mail.date
+        Post.find_or_create_by_message_id(hash)
+        true
+      rescue Error => err
+        pp 'err', err,mail
+        false
+      end
     end
   end
 end
