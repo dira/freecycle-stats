48d7880bca265f5bd34975626263ee0b9cc555ad
Irina Dumitrascu <me@dira.ro>
2009-10-13 00:40:09 +0300

    changed matching algorithm - word based

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -33,18 +33,29 @@ module Matcher
 
     return 1 if s1 == s2
 
-    distance = Text::Levenshtein.distance(s1, s2)
-    lengths = [s1.length, s2.length]
-    sure_distance = lengths.max - lengths.min
+    s1, s2 = s2, s1 if s1.length > s2.length # s1 is the short one
 
-    return false unless distance - sure_distance < lengths.min / 5
+    s1_words = s1.split(/\s+/).select{|word| word.length >= 3}
+    s2_words = s2.split(/\s+/).select{|word| word.length >= 3}
 
-    return -distance
+    words = 0.0
+    s1_words.each do |word|
+      s2_words.each do |candidate|
+        if word.length <= 4 && word == candidate || word.length > 4 && Text::Levenshtein.distance(word, candidate) <= 2
+          words += 1
+          break
+        end
+      end
+    end
+
+    return 0 if words / s1_words.size < 0.34
+    words / s1_words.size + words / s2_words.size
   end
 
   def Post.simplify_subject(subject)
     s1 = subject.downcase
     s1.gsub!(/\([^)]*\)/, '')
+    s1.gsub!(/\[[^\]]*\]/, '')
     s1.strip
   end
 end
--- test/unit/matching_test.rb
+++ test/unit/matching_test.rb
@@ -20,16 +20,62 @@ context 'matching should be weighted' do
 
   should "give better score for match closer to the base's length" do
     base = 'ghete'
-    assert Post.similarity('ghete noi', base) > Post.similarity('ghete cu funda', base)
-  end
-
-  should "return false for no match" do
-    assert_equal false, Post.similarity('ghete noi', 'papuci')
+    assert Post.similarity('ghete noi', base) > Post.similarity('ghete fara sireturi', base)
   end
 
   should 'ignore what is in paranthesys' do
     assert_equal 1, Post.similarity('ghete (zona Floreasca)', 'ghete')
   end
-end
 
+  [
+    ['ghete noi', 'papuci'],
+    ['ghete fara sireturi noi', 'papuci fara talpa']
+  ].each do |s1, s2|
+    should "not recognize #{s1} and #{s2}" do
+      assert_equal 0, Post.similarity(s1, s2)
+    end
+  end
+
+  [
+    ["sterilizator si huda izoterma biberoane",
+     "sterilizator biberoane pt cuptorul cu microunde si o husa termica"],
+
+    ["scaner fara cabluri",
+    "scaner [3 Attachments]"],
+
+    ["carticica pt mamica si cursuri mkt",
+    "brosura pentru viitoare mamici (in Franceza) si cursuri (marketing&PR&publ)"],
+
+    ["monitor functional DELL",
+    "monitor Dell [1 Attachment]"],
+
+    ["derulator de casete video si boxa",
+    "difuzor si derulator de casete video"],
+
+    ["Monitor 17 inch (care tiuie)",
+    "Monitor tiuitor"],
+
+    ["carti si manuale de scoala",
+    "manuale si carti de scoala"],
+
+    ["cedat: abonament internet mobile Vodafone",
+    "Cedez abonament mobile Internet VDF"],
+
+    ["Dulap vechi an 1900",
+     "dulap vechi, 1900, prafuit si fara placa de marmura"],
+
+    ["Aragaz 4 ochiuri si cuptor",
+    "Aragaz cu patru ochiuri in Berceni"],
+
+    ["2 Palete Badminton, 2 Rachete Tenis, o parte din carti vechi",
+    "Carti Vechi, 2 Palete Badminton, 2 Rachete Tenis"],
+
+    ["colectie Top Gun (revista de aviatie)",
+    "reviste Top Gun (750)"]
+  ].each do |s1, s2|
+    should "recognize #{s1} and #{s2} as similar" do
+      assert Post.similarity(s1, s2)
+    end
+  end
+end
 end
