4710691616ac6ed6fb875dc0cc5a657359b6afc4
Irina Dumitrascu <me@dira.ro>
2010-01-06 17:30:41 -0200

    unified by month stats calculation - with and without conditions

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,29 +1,4 @@
 class Stat < ActiveRecord::Base
-
-  def self.messages_per_month(nr_months)
-    start_date = (nr_months - 1).months.ago.beginning_of_month.to_date
-    end_date = 0.days.ago.end_of_month.to_date
-
-    data = {}
-    labels = []
-    Post::KIND_MESSAGES.each{|kind| data[kind] = [] }
-
-    while start_date < end_date
-      labels << start_date
-
-      raw = Post.count(
-        :group  => "kind",
-        :conditions => ["sent_date >= ? AND sent_date <= (?) AND kind IN (?)", 
-                        start_date, start_date.end_of_month, Post::KIND_MESSAGES])
-      counts = {}
-      raw.each{ |m| counts[m[0]] = m[1] }
-      data.each{|kind, value| data[kind] << (counts[kind] || 0) }
-      
-      start_date = start_date.next_month
-    end
-    { :data => data, :labels => labels }
-  end
-
   def self.messages_per_kind
      data, labels, categories = [], [], []
      sorted_frequency = Post.count(:group => :category, :include => :category).map{|k, v| [k, v]}.sort{|a, b| b[1] <=> a[1]}
@@ -35,7 +10,16 @@ class Stat < ActiveRecord::Base
     { :data => data, :labels => labels, :categories => categories }
   end
 
+  def self.messages_per_month(nr_months)
+    messages_in_scope_per_month(nr_months, Post)
+  end
+
   def self.messages_per_kind_per_month(category, nr_months)
+    messages_in_scope_per_month(nr_months, Post.by_category_id(category.id))
+  end
+
+private
+  def self.messages_in_scope_per_month(nr_months, posts)
     start_date = (nr_months - 1).months.ago.beginning_of_month.to_date
     end_date = 0.days.ago.end_of_month.to_date
 
@@ -46,7 +30,7 @@ class Stat < ActiveRecord::Base
     while start_date < end_date
       labels << start_date
 
-      raw = Post.by_category_id(category.id).count(
+      raw = posts.count(
         :group  => "kind",
         :conditions => ["sent_date >= ? AND sent_date <= (?) AND kind IN (?)",
                         start_date, start_date.end_of_month, Post::KIND_MESSAGES])
