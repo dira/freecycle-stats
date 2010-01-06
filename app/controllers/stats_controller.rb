a2d80b9b28acf92acd47549114916edf8e9a3e0c
Irina Dumitrascu <me@dira.ro>
2010-01-06 16:34:06 -0200

    refactored and improved charts

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,10 @@
 class StatsController < ApplicationController
   def index
     Post.categorize_all
-    @global_per_month = Stat.messages_per_month(6)
-    @global_per_kind = Stat.messages_per_kind
+    @per_month = Stat.messages_per_month(6)
+    @per_kind = Stat.messages_per_kind
 
-    @kinds_per_month = @global_per_kind[:categories][0..2].map do |category|
+    @kinds_per_month = @per_kind[:categories][0..2].map do |category|
       [category, Stat.messages_per_kind_per_month(category, 6)]
     end
   end
