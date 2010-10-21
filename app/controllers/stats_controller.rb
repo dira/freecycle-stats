841ee5c9e2a06fe535c80e8010b11f72ada804af
Irina Dumitrascu <me@dira.ro>
2010-10-21 13:46:20 -0400

    Show only main stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,10 +3,10 @@ class StatsController < ApplicationController
     nr_months = 6
     Post.categorize_all
     @per_month = Stat.messages_per_month(nr_months)
-    @per_kind = Stat.messages_per_kind(nr_months)
+    # @per_kind = Stat.messages_per_kind(nr_months)
 
-    @kinds_per_month = @per_kind[:categories].map do |category|
-      [category, Stat.messages_per_kind_per_month(category, nr_months)]
-    end
+    # @kinds_per_month = @per_kind[:categories].map do |category|
+      # [category, Stat.messages_per_kind_per_month(category, nr_months)]
+    # end
   end
 end
