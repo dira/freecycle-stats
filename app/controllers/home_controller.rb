24ce3970cd242c823d195f45bf7689496488862f
Irina Dumitrascu <me@dira.ro>
2010-10-21 18:35:59 -0400

    More info on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @per_month_per_kind = Stat.per_month_per_kind(6)
+    @offers   = Post.order("sent_date desc").limit(5).where(:kind => 'offer')
+    @requests = Post.order("sent_date desc").limit(5).where(:kind => 'request')
   end
 end
